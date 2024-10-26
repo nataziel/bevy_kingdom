c1509ab2d3e9eaf66ab84891134b7068d87ee7e6
nataziel <aerofuel@gmail.com>
2024-10-26 21:58:48 +1000

    added first implementation of parents and children

--- src/people.rs
+++ src/people.rs
@@ -9,37 +9,135 @@ struct Name {
     last: String,
 }
 
+#[derive(Component, Debug)]
+struct Parents {
+    list: Vec<Entity>,
+}
+
+#[derive(Component, Debug)]
+struct Children {
+    list: Vec<Entity>,
+}
+
 fn hello_world() {
     info!("Hello world!");
 }
 
 fn add_people(mut commands: Commands) {
-    commands.spawn((
-        Person,
-        Name {
-            first: "Jack".to_string(),
-            last: "Allan".to_string(),
-        },
-    ));
-    commands.spawn((
-        Person,
-        Name {
-            first: "Paulina".to_string(),
-            last: "Morales".to_string(),
-        },
-    ));
-    commands.spawn((
-        Person,
-        Name {
-            first: "Albert".to_string(),
-            last: "Morales-Allan".to_string(),
-        },
-    ));
+    let jack = commands
+        .spawn((
+            Person,
+            Name {
+                first: "Jack".to_string(),
+                last: "Allan".to_string(),
+            },
+        ))
+        .id();
+
+    let pau = commands
+        .spawn((
+            Person,
+            Name {
+                first: "Paulina".to_string(),
+                last: "Morales".to_string(),
+            },
+        ))
+        .id();
+
+    let albie = commands
+        .spawn((
+            Person,
+            Name {
+                first: "Albert".to_string(),
+                last: "Morales-Allan".to_string(),
+            },
+            Parents {
+                list: vec![jack, pau],
+            },
+        ))
+        .id();
+
+    let pip = commands
+        .spawn((
+            Person,
+            Name {
+                first: "Pip".to_string(),
+                last: "Morales-Allan".to_string(),
+            },
+            Parents {
+                list: vec![jack, pau],
+            },
+        ))
+        .id();
+
+    commands.entity(jack).insert(Children {
+        list: vec![albie, pip],
+    });
+    commands.entity(pau).insert(Children {
+        list: vec![albie, pip],
+    });
+
+    let jacob = commands
+        .spawn((
+            Person,
+            Name {
+                first: "Jacob".to_string(),
+                last: "Wilmot".to_string(),
+            },
+        ))
+        .id();
+
+    let pepsi = commands
+        .spawn((
+            Person,
+            Name {
+                first: "Pepsi".to_string(),
+                last: "Wilmot".to_string(),
+            },
+            Parents { list: vec![jacob] },
+        ))
+        .id();
+
+    commands
+        .entity(jacob)
+        .insert(Children { list: vec![pepsi] });
 }
 
 fn greet_people(query: Query<&Name, With<Person>>) {
     for name in &query {
         info!("Hello {} {}!", name.first, name.last)
+    }
+}
+
+fn greet_people_with_children(
+    query_parents: Query<(&Name, &Children), With<Person>>,
+    query_children: Query<(&Name, &Parents), With<Person>>,
+) {
+    for (name, children) in &query_parents {
+        info!("Hello {} {}!", name.first, name.last);
+
+        for (child_name, _) in query_children.iter_many(&children.list) {
+            info!(
+                "{} {} has a child called {} {}",
+                name.first, name.last, child_name.first, child_name.last,
+            )
+        }
+    }
+}
+
+fn greet_people_with_parents(
+    query_parents: Query<(&Name, &Children), With<Person>>,
+    query_children: Query<(&Name, &Parents), With<Person>>,
+) {
+    for (name, parents) in &query_children {
+        info!("Hello {} {}!", name.first, name.last);
+
+        for (parent_name, _) in query_parents.iter_many(&parents.list) {
+            info!(
+                "{} {} has a parent called {} {}",
+                name.first, name.last, parent_name.first, parent_name.last,
+            )
+        }
     }
 }
 
@@ -57,6 +155,18 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, add_people);
-        app.add_systems(Update, (hello_world, (update_people, greet_people).chain()));
+        app.add_systems(
+            Update,
+            (
+                hello_world,
+                (
+                    update_people,
+                    greet_people,
+                    greet_people_with_children,
+                    greet_people_with_parents,
+                )
+                    .chain(),
+            ),
+        );
     }
 }
